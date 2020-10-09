63c47ae86af22cc5fa7a4f521c7700588e08d107
ghemulyanto <trimulyanto.tm@gmail.com>
2020-10-09 15:16:42 +0700

    Commit

---
+++ src/components/RadioSample.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-container>
+    <v-row class="text-center">
+      <div v-for="n in 3" :key="n">
+        {{ `radioGroup${n}` }}
+        <v-col cols="12">
+          <v-radio-group :v-model="`radioGroup${n}`" :mandatory="false">
+            <v-radio :label="`Chicago Bulls ${n}`" :value="`Bulls ${n}`"></v-radio>
+            <v-radio :label="`Houston Rockets ${n}`" :value="`Rockets ${n}`"></v-radio>
+          </v-radio-group>
+        </v-col>
+        <v-btn :disabled="`radioGroup${n}`">Click</v-btn>
+      </div>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      radioGroup: false,
+    };
+  },
+};
+</script>
